34a490adaf60d65665af180d22253dd0989d3b05
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-04-15 00:57:35 +0900

    initial commit

---
+++ src/main/java/com/rockpago/yestion/YestionApplication.java
@@ -0,0 +1,13 @@
+package com.rockpago.yestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rockpago/yestion/YestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockpago.yestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
