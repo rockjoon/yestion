513879711bbe61ab526e3b3c7808e7166dced660
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-04-25 23:48:56 +0900

    test : Time Entity test 추가

---
+++ src/test/java/com/rockpago/yestion/time/domain/TimeTest.java
@@ -0,0 +1,25 @@
+package com.rockpago.yestion.time.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TimeTest {
+
+    @DisplayName("Time Entity Builder 확인")
+    @Test
+    void build() {
+        int minutes = 30;
+        int hours = 1;
+
+        Time time = Time.builder()
+                .hours(hours)
+                .minutes(minutes)
+                .build();
+
+        assertThat(time.getHours()).isEqualTo(hours);
+        assertThat(time.getMinutes()).isEqualTo(minutes);
+    }
+
+}
